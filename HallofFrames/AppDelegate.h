23b47e6a85744ebecf39a8a0f160a87404d2632d
Ceasar Barbosa <CeazB@ceasars-macbook-pro.local>
2016-01-20 14:00:00 -0600

    Initial Commit

---
+++ HallofFrames/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallofFrames
+//
+//  Created by Ceasar Barbosa on 1/20/16.
+//  Copyright © 2016 MobileMakers. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
